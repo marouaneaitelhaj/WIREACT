80e3fd30ec1fda17098a53ad6d2e46588f440315
marouaneaitelhaj <marouaneaitelhaj001@gmail.com>
2023-05-10 17:19:09 -0700

    update

--- App.js
+++ App.js
@@ -10,81 +10,92 @@ import { faP } from "@fortawesome/free-solid-svg-icons/faP";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 // import component
 import BookingScreen from "./Component/Booking";
 import SearchParking from "./Component/SearchParking";
 import HistoryScreen from "./Component/History";
 import ProfileScreen from "./Component/Profile";
 import Maps from "./Component/maps";
+import { useTheme } from 'react-native-paper';
+
+
 export default function App() {
+  const theme = useTheme();
+// theme.colors.secondaryContainer = "transparent";
   const Stack = createNativeStackNavigator();
   const Tab = createMaterialBottomTabNavigator();
   return (
-    <View style={tw.style("w-full h-full flex ")}>
-      <NavigationContainer>
-        <Tab.Navigator style={tw.style("")}>
-          <Tab.Screen
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesomeIcon
-                  style={tw.style("text-gray-500")}
-                  icon={faPaperPlane}
-                />
-              ),
-            }}
-            name="Nearby"
-            component={Maps}
-          />
-          <Tab.Screen
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesomeIcon
-                  style={tw.style("text-gray-500")}
-                  icon={faBookmark}
-                />
-              ),
-            }}
-            name="Booking"
-            component={BookingScreen}
-          />
-          <Tab.Screen
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesomeIcon
-                  style={tw.style("text-gray-500")}
-                  icon={faP}
-                />
-              ),
-            }}
-            name="Search Parking"
-            component={SearchParking}
-          />
-          <Tab.Screen
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesomeIcon
-                  style={tw.style("text-gray-500")}
-                  icon={faClockRotateLeft}
-                />
-              ),
-            }}
-            name="History"
-            component={HistoryScreen}
-          />
-          <Tab.Screen
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <FontAwesomeIcon
-                  style={tw.style("text-gray-500")}
-                  icon={faUser}
-                />
-              ),
-            }}
-            name="Profile"
-            component={ProfileScreen}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer 
+    theme={
+      {
+        colors: {
+          secondaryContainer: 'transparent',
+        }
+      }
+    }
+    >
+      <Tab.Navigator
+        initialRouteName="Nearby"
+        barStyle={{ backgroundColor: "#ffffff" }}
+        activeColor="#24aaa1"
+        inactiveColor="gray"
+        shifting={true}
+         
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color }) => (
+              // <FontAwesomeIcon color={color} icon={faPaperPlane} />
+              <Ionicons name="home" color={color} size={26} />
+            ),
+          }}
+          name="Nearby"
+          component={Maps}
+        />
+        <Tab.Screen
+        
+
+          options={{
+            tabBarIcon: ({ color }) => (
+              // <FontAwesomeIcon color={color} icon={faBookmark} />
+              <Ionicons name="bookmark" color={color} size={26} />
+            ),
+          }}
+          name="Booking"
+          component={BookingScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              // <FontAwesomeIcon color={color} icon={faP} />
+              <Ionicons name="search" color={color} size={26} />
+            ),
+          }}
+          name="Search Parking"
+          component={SearchParking}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon color={color} size={26} icon={faClockRotateLeft} />
+              // <Ionicons name="history" color={color} size={26} />
+            ),
+          }}
+          name="History"
+          component={HistoryScreen}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              // <FontAwesomeIcon color={color} icon={faUser} />
+              <Ionicons name="person" color={color} size={26} />
+            ),
+          }}
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
